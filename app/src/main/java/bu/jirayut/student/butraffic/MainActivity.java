3b6ac3ab6d3b3efe00b4d695563833acc9297841
jirayut <jirayut@gmail.com>
2015-11-14 16:03:04 +0700

    Finish Day1

--- app/src/main/java/bu/jirayut/student/butraffic/MainActivity.java
+++ app/src/main/java/bu/jirayut/student/butraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package bu.jirayut.student.butraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 // Sound Effect
                 MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
                 buttonPlayer.start();
+
+                // Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://androidthai.in.th/"));
+                startActivity(objIntent);
 
             }   // event
         });
